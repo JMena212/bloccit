954a03d2844b3e7c58d6b757a71e83137e073a7e
Jose Mena <josemena0114@gmail.com>
2016-08-31 11:50:46 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-   get 'welcome/index'
+ #1  we call the resources method and pass it a Symbol. 
+ #This instructs Rails to create post routes for creating, updating, viewing, and deleting instances of Post. 
+ #We'll review the precise URIs created in a moment.
+   resources :posts
 
-   get 'welcome/about'
+   get 'about' => 'welcome#about'
    
    root 'welcome#index'
 
