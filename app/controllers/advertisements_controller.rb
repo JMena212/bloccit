a64978c6e68aa7e320d52b9745860f135c57bb9b
Jose Mena <josemena0114@gmail.com>
2016-09-06 17:32:42 +0000

    assignment-32-complete

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -1,13 +1,31 @@
 class AdvertisementsController < ApplicationController
   def index
+    @advertisements = Advertisement.all
   end
 
   def show
+    @advertisement = Advertisement.find(params[:id])
   end
 
   def new
+    @advertisements = Advertisement.new
   end
 
   def create
+     @advertisements = Advertisement.new
+     @advertisements.title = params[:advertisement][:title]
+     @advertisements.body = params[:advertisement][:body]
+
+ # #10
+     if @advertisements.save
+ # #11
+       flash[:notice] = "advertisement was saved successfully."
+       redirect_to @advertisements
+     else
+ # #12
+       flash.now[:alert] = "There was an error saving the advertisement. Please try again."
+       render :new
+     end
   end
 end
+    
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,11 @@
 Rails.application.routes.draw do
-  get 'advertisements/index'
-
-  get 'advertisements/show'
-
-  get 'advertisements/new'
-
-  get 'advertisements/create'
-
-  get 'index/show'
-
-  get 'index/new'
-
-  get 'index/create'
-
- #1  we call the resources method and pass it a Symbol. 
- #This instructs Rails to create post routes for creating, updating, viewing, and deleting instances of Post. 
- #We'll review the precise URIs created in a moment.
    resources :posts
    
    resources :advertisements
 
    get 'about' => 'welcome#about'
    
-   root 'welcome#index'
+   root to: 'welcome#index'
 
 end
 
