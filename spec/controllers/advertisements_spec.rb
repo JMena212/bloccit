6180421aa44638017caf026a097bf79966445dc5
Jose Mena <josemena0114@gmail.com>
2016-09-06 15:09:57 +0000

    removing incorrectly generated index controller

--- spec/controllers/advertisements_spec.rb
+++ spec/controllers/advertisements_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe IndexController, type: :controller do
+RSpec.describe AdvertisementsController, type: :controller do
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
 
   describe "GET #show" do
     it "returns http success" do
