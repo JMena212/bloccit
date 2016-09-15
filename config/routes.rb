d37ca0a4bf859edcc6856448bf73ea396b6376e0
Jose Mena <josemena0114@gmail.com>
2016-09-15 01:39:56 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
  #1  we call the resources method and pass it a Symbol. 
  #This instructs Rails to create post routes for creating, updating, viewing, and deleting instances of Post. 
  #We'll review the precise URIs created in a moment.
+    resources :topics
    resources :posts
 
    get 'about' => 'welcome#about'
