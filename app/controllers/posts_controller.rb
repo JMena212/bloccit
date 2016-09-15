873f0d70f937c39a6726e75550ca249a22f625b6
Jose Mena <josemena0114@gmail.com>
2016-09-15 03:06:58 +0000

    assignment-34-complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,11 @@
 class PostsController < ApplicationController
-  def index
-   # #11
-     @posts = Post.all
-  end
-
   def show
    # #19
      @post = Post.find(params[:id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
     @post = Post.new
   end
 
@@ -24,7 +20,7 @@ class PostsController < ApplicationController
  
      if @post.save
        flash[:notice] = "Post was updated successfully."
-       redirect_to @post
+         redirect_to [@post.topic, @post]
      else
        flash.now[:alert] = "There was an error saving the post. Please try again."
        render :edit
@@ -37,12 +33,15 @@ class PostsController < ApplicationController
      @post = Post.new
      @post.title = params[:post][:title]
      @post.body = params[:post][:body]
+     @topic = Topic.find(params[:topic_id])
+ # #35
+     @post.topic = @topic
 
  # #10
      if @post.save
  # #11
        flash[:notice] = "Post was saved successfully."
-       redirect_to @post
+       redirect_to [@topic, @post]
      else
  # #12
        flash.now[:alert] = "There was an error saving the post. Please try again."
@@ -57,7 +56,7 @@ class PostsController < ApplicationController
  # #8
      if @post.destroy
        flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-       redirect_to posts_path
+       redirect_to @post.topic
      else
        flash.now[:alert] = "There was an error deleting the post."
        render :show
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
  #1  we call the resources method and pass it a Symbol. 
  #This instructs Rails to create post routes for creating, updating, viewing, and deleting instances of Post. 
  #We'll review the precise URIs created in a moment.
-    resources :topics
-   resources :posts
+    resources :topics do
+ # #34
+     resources :posts, except: [:index]
+   end
+
 
    get 'about' => 'welcome#about'
    
