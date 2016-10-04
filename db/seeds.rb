46a22768368deff81bdc60e874ff4a1783bbc036
Jose Mena <josemena0114@gmail.com>
2016-10-04 02:45:00 +0000

    checkpoint-40-rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,12 +44,20 @@ require 'random_data'
    )
  end
  
- user = User.first
- user.update_attributes!(
-   email: 'josemena0114@gmail.com', # replace this with your personal email
-   password: 'helloworld'
+# Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
  )
  
+ # Create a member
+   member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
  
  puts "Seed finished"
  puts "#{User.count} users created"
