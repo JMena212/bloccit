990c5a27554c1d55feb175974a0af8b0ed157ef7
Jose Mena <josemena0114@gmail.com>
2016-10-20 03:29:46 +0000

    first commit

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -15,5 +15,16 @@ class FavoriteMailer < ApplicationMailer
  # #19
      mail(to: user.email, subject: "New comment on #{post.title}")
    end
+   
+   def new_post(post)
+       headers["Message-ID"] = "<posts/#{post.id}@Bloccit.example>"
+       headers["Message-ID"] = "<posts/#{post.id}@Bloccit.example>"
+       headers["Message-ID"] = "<posts/#{post.id}@Bloccit.example>"
+       
+       @post = post
+       
+       mail(to: post.user.email, subject: "You favorited, #{post.title}!")
+       
+   end
 
 end
