4881c1bed76b7c33b858cbdc3e71a91e13b2e10e
Jose Mena <josemena0114@gmail.com>
2016-10-24 03:09:39 +0000

    last commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,14 @@ class User < ActiveRecord::Base
  # #3
    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
  # #4
-  validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
-  validates :password, length: { minimum: 6 }, allow_blank: true
+  
  # #5
    validates :email,
              presence: true,
              uniqueness: { case_sensitive: false },
              length: { minimum: 3, maximum: 254 }
+  validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
+  validates :password, length: { minimum: 6 }, allow_blank: true
 
  # #6
    has_secure_password
