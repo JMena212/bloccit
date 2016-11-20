0e10353eba07d1872c08cbb308b7f92acc3a8222
Jose Mena <josemena0114@gmail.com>
2016-11-20 19:50:24 +0000

    checkpoint-49-complete

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -26,6 +26,15 @@ class Api::V1::UsersController < Api::V1::BaseController
    end
  
    def create
+       user = User.new(user_params)
+ 
+ # #14
+     if user.valid?
+       user.save!
+       render json: user, status: 201
+     else
+       render json: { error: "User is invalid", status: 400 }, status: 400
+     end
    end
    
     private
--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,8 @@ Rails.application.routes.draw do
    namespace :api do
      namespace :v1 do
        resources :users, only: [:index, :show, :create, :update]
-       resources :topics, only: [:index, :show]
-     end
+       resources :topics, except: [:edit, :new]
+   end
    end
    
  # #4
